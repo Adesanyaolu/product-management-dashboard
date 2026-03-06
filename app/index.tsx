8db45b8f646d730d8c9968edb24b872611a71784
adesanyaolu <adenugaadesanyao@gmail.com>
2026-03-06 11:28:22 +0100

    feat: replace default index screen with HomeScreen component

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,7 @@
-import { Text, View } from "react-native";
+import HomeScreen from "./(tabs)";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-red-900">Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <HomeScreen />
   );
 }
